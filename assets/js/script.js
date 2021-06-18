0ee271fb0e283312bb139d3d4357c8838aa36b8f
Dana Merrill <dtmerrilliii@gmail.com>
2021-06-17 20:24:45 -0400

    Updates to start local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,20 +16,44 @@ var issueContainer = document.getElementById('issues');
 var clouds = document.createElement('h3');
 var temp = document.createElement('h3');
 var wind = document.createElement('h3');
+var ultraviolet = document.createElement('h3');
 var cityString
 var requestAPI
 var fetchButton = document.getElementById('fetch-button');
 var weatherEl = document.getElementById('weather1');
-
-
-
 var weatherButton = document.getElementById("weatherButton")
-var userInputEl = document.getElementById("userInput")
+var cityInputEl = document.getElementById("cityInput")
+
 function weather() {
 }
 
 var city
 console.log(city)
+
+function renderCities() {
+  cityList.innerHTML = ""
+}
+function renderSearchHistory() {
+  searchHistoryContainer.innerHTML = ""
+  // for loop that explores search history length
+  // button.set attribute
+}
+//
+
+function appendHistory() {
+  if (searchHistory.indexOf(search) !== -1) {
+    return
+  }
+  searchHistory.push(search)
+  localStorage.setItem("search-history", JSON.stringify(searchHistory));
+  renderSearchHistory();
+}
+
+
+function storeCity() {
+  localStorage.setItem("city", JSON.stringify(city));
+}
+storeCity();
   
 if (city != null) {
     console.log("function")
@@ -38,28 +62,36 @@ if (city != null) {
    }
 
 function getCity(){
-
-    
        console.log("click")
-       var city = userInputEl.value
-       console.log(userInputEl.value)
+       var city = cityInputEl.value
+       console.log("city name",cityInputEl.value)
        localStorage.setItem("city", JSON.stringify(city));
        
      
        console.log(city)
        requestAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f57cc3d88487e632b111d5d350ce8f21&units=imperial";
        getWeather();
-
 };
-weatherButton.addEventListener("click", getCity)   
+weatherButton.addEventListener("click", getCity)
 
+function getLocation(){
+  var city = cityInputEl.value
+  console.log("city name",cityInputEl.value)
+  localStorage.setItem("city", JSON.stringify(city));
+  
+
+  console.log(city)
+  requestAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f57cc3d88487e632b111d5d350ce8f21&units=imperial";
+  getWeather();
+};
+// function to fetch openAPI with lon/lat
 
      
-   if (city != null) {
-    console.log(city)
-    requestAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f57cc3d88487e632b111d5d350ce8f21&units=imperial";
-       getWeather();
-   }
+  //  if (city != null) {
+  //   console.log(city)
+  //   requestAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f57cc3d88487e632b111d5d350ce8f21&units=imperial";
+  //      getWeather();
+  //  }
    
 function getWeather() {
     fetch(requestAPI)
@@ -69,15 +101,15 @@ function getWeather() {
       .then(function (data) {
        console.log(data)
         console.log("request for data code is " + data.weather[0].id + " and requested icon is " + data.weather[0].icon);
-        var code = data.weather[0].id
- //       matchIngredient(code)
   
         clouds.textContent = data.weather[0].description;
-        temp.textContent = "a temperature of " + data.main.temp + " degrees farenheit";
-        wind.textContent = "and an average wind speed of " + data.wind.speed + " mph";
+        temp.textContent = "the temperature is " + data.main.temp + " degrees farenheit (and it feels like " + data.main.feels_like + " degrees)";
+        wind.textContent = "with an average wind speed of " + data.wind.speed + " mph";
+    //    ultraviolet.textContent = "with a UV index of " + current.uvi
         issueContainer.append(clouds);
         issueContainer.append(temp);
         issueContainer.append(wind);
+        issueContainer.append(ultraviolet);
         myWeather = data.weather[0].description
     });
   }
